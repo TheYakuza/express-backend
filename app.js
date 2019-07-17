d590a5576863a9088fbec6b3ae23670faa319cb1
Nicolas Martinez <nmartinezv@icloud.com>
2019-07-17 11:00:25 -0400

    fix remove swagger integration

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const expressSession = require('express-session');
 
 const app = express();
 
-const expressSwagger = require('express-swagger-generator')(app);
-
-
 // const corsOptions = require('@/etc/corsConfig');
 const { connectDB: db } = require('@/etc/dbConfig');
 
@@ -61,33 +58,6 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
-const options = {
-  swaggerDefinition: {
-    info: {
-      description: 'This is a sample server',
-      title: 'Swagger',
-      version: '1.0.0',
-    },
-    host: `${host}:${port}`,
-    basePath: '/api',
-    produces: [
-      'application/json',
-    ],
-    schemes: ['http', 'https'],
-    securityDefinitions: {
-      JWT: {
-        type: 'apiKey',
-        in: 'header',
-        name: 'Authorization',
-        description: '',
-      },
-    },
-  },
-  basedir: __dirname, // app absolute path
-  files: ['./routes/*.js'], // Path to the API handle folder
-};
-
-expressSwagger(options);
 
 app.listen(port || 3000, () => {
   log().info(msg.server, host, port, nodeEnv);
